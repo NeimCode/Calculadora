90cd22b8055a74d085532a7100b1cca966522fcc
NeimCode <Cutyshyhealt@gmail.com>
2025-03-24 16:58:18 +0100

    Vamos a pretender que he arreglado algo sin romper 3 cosas más.

--- javascript/script.js
+++ javascript/script.js
@@ -1,15 +1,33 @@
 function limpiarNum() {
     document.getElementById('display').value = '0'; //Limpia el contenido a 0, básicamente es el clear, haha '-'.
 }
-function numeroAEnsenyar(value) { //En esto se hace la magia, se le pasa el valor al display, y si el display es 0, pues se le pasa el valor.
-    var sound = new  Audio("meowrgh.mp3");  
-    sound.play();
-    let display = document.getElementById('display');
-    if (display.value === '0') { //Si el display es 0
-        display.value = value; //Si es solo 0, se queda como 0
-    } else {
-        display.value += value; //Si no es solo 0, entonces se puede añadir.
-    }
+
+
+
+
+    function numeroAEnsenyar(value) { //En esto se hace la magia, se le pasa el valor al display, y si el display es 0, pues se le pasa el valor.
+        var sound = new  Audio("meowrgh.mp3");  
+        sound.play();
+        let display = document.getElementById('display');
+    let ultimoCaracter = display.value.slice(-1);
+        if (display.value === '0') {
+            display.value = value; 
+        } else {
+            if(['+', '*', '/', '-', '.'].includes(value) && ['+', '*', '/', '-', '.'].includes(ultimoCaracter)) {
+                return;   
+            }
+            }
+            display.value += value; 
+        
+        
+    
+    //Si hay un símbolo como +, *, - o /, nos interesa que no se repita consecutivamente, ya que no es lo mismo hacer 6--2, que -6-2
+    //¿Entonces cómo podemos saberlo? que no se repita el numero... 
+    
+
+    
+    
+    
 }
 function calcularResultado() {
     let display = document.getElementById('display'); //Pasa el display
